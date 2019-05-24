6e0561da29513793cb72e196f30bac7868509760
10veda <36359963+10veda@users.noreply.github.com>
2019-05-24 10:11:57 +0530

    Create mirror clock.java

---
+++ mirror clock.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+	public static void main (String[] args) {
+	    		Scanner in=new Scanner(System.in);
+	    		int t=in.nextInt();
+	    		while(t-->0)
+	    		{
+	    		    String s=in.next();
+	    		    int h=Integer.parseInt(s.substring(0,2));
+	    		    int m=Integer.parseInt(s.substring(3));
+	    		    
+	    		    m=(60-m)%60;
+	    		    h=12-h;
+				
+	    		    if(m!=0)
+	    		    --h;
+				
+	    		    h=(h+12)%12;
+				
+	    		    if(h==0)
+	    		    h=12;
+	    		    System.out.printf("%02d:%02d\n", h, m);
+	    		}
+	}
+}
